5fd9464f135484bf8969ba64ad5329a1ce97b23f
Anton Schastny <schaan@ukr.net>
2011-05-28 12:01:25 +0300

    Убрал лишние комменты

--- src/main/java/net/schastny/intender/dao/UserDAO.java
+++ src/main/java/net/schastny/intender/dao/UserDAO.java
@@ -3,7 +3,6 @@ package net.schastny.intender.dao;
 import net.schastny.intender.domain.TenderUser;
 
 public interface UserDAO {
-	
 	public void storeUser(TenderUser user);
 	public void deleteUser(String username);
 	public TenderUser getUser(String username);
--- src/main/java/net/schastny/intender/service/DivisionService.java
+++ src/main/java/net/schastny/intender/service/DivisionService.java
@@ -5,18 +5,8 @@ import java.util.List;
 import net.schastny.intender.domain.Division;
 
 public interface DivisionService {
-	
-	// админ: добавить категорию
-	// админ: редактировать категорию
 	public void storeDivision(Division division);
-
-	// админ: удалить категорию
 	public void deleteDivision(Integer id);
-
-	// все: список категорий
 	public List<Division> showAll();
-	
-	//все:	категория детально
 	public Division showDivision(Integer id);
-
 }
